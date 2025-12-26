6e50dad8a0a79735ce414f447a58029ba814c438
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-26 17:20:01 +0200

    03 - Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   return (
       <>
+        <Navbar/>
         <Hero/>
         <ShowcaseSection/>
       </>
