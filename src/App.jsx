4d4abcea1108a74e60ef5294c5568c7ca579d7d6
Tsvetanmarkov <tsvetan.markov.a70@learn.telerikacademy.com>
2025-12-26 17:36:04 +0200

    04: LogoSection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import Navbar from "./components/Navbar.jsx";
+import LogoSection from "./components/LogoSection.jsx";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
         <Navbar/>
         <Hero/>
         <ShowcaseSection/>
+        <LogoSection/>
       </>
   )
 }
